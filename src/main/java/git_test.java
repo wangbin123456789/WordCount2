bdc0a111592ac429889bbf5386f4a5baca34e05b
wangbin <wb493404732@163.com>
2022-01-05 11:24:50 +0800

    master commit

--- src/main/java/git_test.java
+++ src/main/java/git_test.java
@@ -2,5 +2,6 @@ public class git_test {
     public static void main(String[] args) {
         System.out.println("123");
         System.out.println("aaa");
+        System.out.println("master");
     }
 }
