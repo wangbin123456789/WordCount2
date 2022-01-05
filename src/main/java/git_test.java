744a3f1a057492e84e91a4fb9e52fd89d79f7cd0
wangbin <wb493404732@163.com>
2022-01-05 11:50:49 +0800

    push commit

--- src/main/java/git_test.java
+++ src/main/java/git_test.java
@@ -4,5 +4,6 @@ public class git_test {
         System.out.println("aaa");
         System.out.println("master");
         System.out.println("hot-fix");
+        System.out.println("push");
     }
 }
